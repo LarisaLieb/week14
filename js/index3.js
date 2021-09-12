1913270e1bc28f67196ef7b26d0e95981d9cc444
LarisaLieb <larisamaximovna@gmail.com>
2021-09-12 15:55:56 +0200

    галерея

---
+++ js/index3.js
@@ -0,0 +1,10 @@
+function shownext(params) {
+    let imgcat = document.getElementById ('imgcat');
+    imgcat.src ="/img/gc.jpg"
+    
+}
+function showlast(params) {
+    let imgcat = document.getElementById ('imgcat');
+    imgcat.src ="/img/bc.jpg"
+    
+}
